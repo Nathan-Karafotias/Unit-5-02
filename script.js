ca453ec5e04fe237fbcf10891f148d64ef28fbc1
NathanKarafotia <nathan.karafotias@ocsbstudent.ca>
2021-02-24 13:03:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function turnoff() {
+  document.getElementById("paragraph").innerHTML = "The light is now off";
+  return "pic_bulboff.gif";
+}
+
+
+function turnon() {
+  document.getElementById("paragraph").innerHTML = "The light is now on";
+  return "pic_bulbon.gif";
+  document.getElementById('background').style.backgroundColor = "blueviolet"
+}
